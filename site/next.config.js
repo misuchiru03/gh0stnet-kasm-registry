573e48fd1c09cd300d8a0177ba3647377c2403b0
misuchiru03 <misuchiru03+void@gmail.com>
2025-02-07 21:04:24 -0600

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'gh0stnet',
+    description: 'The official gh0stnet store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://misuchiru03.github.io/gh0stnet-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/gh0stnet-registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
